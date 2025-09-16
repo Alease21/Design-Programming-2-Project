e6ddf99eb223f34a252c81689c1c8e7a5a446c82
Alease21 <alease21@outlook.com>
2025-09-15 23:12:03 -0500

    Small Input Change
    -Swapped to a different way to call new input system (shown in class)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,19 +3,50 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private CharacterController _characterController;
-    private InputAction _moveAction;
-
-    [Range(0, 10)]
+    [Range(0, 20)]
     [SerializeField] private float _playerSpeed;
-    void Start()
+    private Vector2 _move;
+    private Rigidbody _rb;
+
+    //value read input
+    public void OnMove(InputAction.CallbackContext context)
     {
-        _characterController = GetComponent<CharacterController>();
-        _moveAction = InputSystem.actions.FindAction("Move");
+        _move = context.ReadValue<Vector2>();
+    }
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
     }
 
-    void Update()
+    private void FixedUpdate()
     {
-        _characterController.Move(_moveAction.ReadValue<Vector2>().normalized * _playerSpeed * 0.01f); //float value is just from trial & error
+        Vector3 currentVelo = _rb.linearVelocity;
+        Vector3 targetVelo = new Vector3(_move.x, _move.y, 0f) * _playerSpeed;
+
+        targetVelo = transform.TransformDirection(targetVelo);
+        Vector3 veloChange = targetVelo - currentVelo;
+
+        _rb.AddForce(new Vector3(veloChange.x, veloChange.y, 0), ForceMode.VelocityChange); //forcemode velocitychange is mass agnostic
+                                                                                            //also can split this to ignore y if gravity is odd
     }
 }
+
+/* Other movement system I had before in class example
+ * keeping here just in case 
+ * 
+private CharacterController _characterController;
+private InputAction _moveAction;
+
+[Range(0, 10)]
+[SerializeField] private float _playerSpeed;
+void Start()
+{
+    _characterController = GetComponent<CharacterController>();
+    _moveAction = InputSystem.actions.FindAction("Move");
+}
+
+void Update()
+{
+    _characterController.Move(_moveAction.ReadValue<Vector2>().normalized * _playerSpeed * 0.01f); //float value is just from trial & error
+}
+*/
