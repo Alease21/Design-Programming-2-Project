4bddb50e2f4c268c5a3692868d59d6e3e7f7cd30
Alease21 <alease21@outlook.com>
2025-11-19 09:59:19 -0600

    11-19 workday
    slight changes to some photon stuff and how enemies target players

--- Assets/Scripts/EnemyScripts/Enemy Base.cs
+++ Assets/Scripts/EnemyScripts/Enemy Base.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
 using static MagicManager;
@@ -9,7 +10,8 @@ public class EnemyBase : MultiplayerObjBase, IEnemy
     public EnemySO baseStats;
     public List<EnemyAttackSO> attacks;
     public EnemyAttackSO currentAttack;
-    public GameObject targetPlayer;
+    public Dictionary<GameObject, float> playersDict = new();
+    public GameObject curTarget = null;
     public UnityEvent OnDeath;
     public bool canAttack = true;
     public bool attackCooldown = true;
@@ -23,42 +25,58 @@ public class EnemyBase : MultiplayerObjBase, IEnemy
         base.Awake();// call base Awake from MultiplayerGameObject for setting GUID & maybe other stuff eventually
         
         curHP = baseStats.hp;
-        targetPlayer = GameObject.FindGameObjectWithTag("Player");
+
+
         if (baseStats.charSprite != null)
         {
             spriteRenderer = GetComponent<SpriteRenderer>();
             spriteRenderer.sprite = baseStats.charSprite;
         }
     }
-
     private void Update()
     {
-        if(spriteRenderer.sprite != baseStats.charSprite)
+        if (curTarget == null)
         {
-            spriteRenderer.sprite = baseStats.charSprite;
+            Debug.Log(GameObject.FindGameObjectsWithTag("Player").Length);
+
+            foreach (var player in GameObject.FindGameObjectsWithTag("Player"))
+                playersDict.Add(player, Vector2.Distance(transform.position, player.transform.position));
+            Debug.Log(playersDict.Count);
+
+            if (playersDict.Count == 0) return;
         }
+
+        for (int i = 0; i < playersDict.Count; i++)
+            playersDict[playersDict.ElementAt(i).Key] = Vector2.Distance(transform.position, playersDict.ElementAt(i).Key.transform.position);
+        var sortedPlayers = playersDict.OrderBy((x) => x.Value);
+        curTarget = sortedPlayers.First().Key;
+
+        if (spriteRenderer.sprite != baseStats.charSprite)
+            spriteRenderer.sprite = baseStats.charSprite;
+
         if (currentAttack == null)
         {
             int curAttack = Random.Range(0,attacks.Count);
             currentAttack = attacks[curAttack];
         }
-        if (targetPlayer != null)
+
+        if (curTarget != null) //or not downed?
         {
-            if (Vector2.Distance(transform.position, targetPlayer.transform.position) >= currentAttack.attackRange)
-            {
-                transform.position = Vector2.MoveTowards(transform.position, targetPlayer.transform.position, baseStats.speed * Time.deltaTime);
-            }
+            if (Vector2.Distance(transform.position, curTarget.transform.position) >= currentAttack.attackRange)
+                transform.position = Vector2.MoveTowards(transform.position, curTarget.transform.position, baseStats.speed * Time.deltaTime);
             else if (canAttack)
-            {
                 if (currentAttack.isProjectile)
-                {
-                    StartCoroutine(UseProjAttack(currentAttack, targetPlayer));
-                }
-            }
+                    StartCoroutine(UseProjAttack(currentAttack, curTarget));
         }
-        else
+        else //if downed/null downed
         {
-            targetPlayer = GameObject.FindGameObjectWithTag("Player");
+            /* check for downed and remove from temp?
+             * 
+            for (int i = 0; i < playersDict.Count; i++)
+                playersDict[playersDict.ElementAt(i).Key] = Vector2.Distance(transform.position, playersDict.ElementAt(i).Key.transform.position);
+            var sortedPlayers2 = playersDict.OrderBy((x) => x.Value);
+            curTarget = sortedPlayers.ElementAt(0).Key;
+            */
         }
 
         //LookAtPlayer();
@@ -66,7 +84,7 @@ public class EnemyBase : MultiplayerObjBase, IEnemy
 
     private void LookAtPlayer()
     {
-        if (transform.position.x < targetPlayer.transform.position.x)
+        if (transform.position.x < curTarget.transform.position.x)
         {
             spriteRenderer.flipX = false;
         }
